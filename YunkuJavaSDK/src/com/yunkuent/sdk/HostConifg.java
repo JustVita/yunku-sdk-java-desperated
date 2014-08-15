2371fcb571b5c1a062a4020e33481920d46d5354
BrandonGk <zb@gokuai.com>
2014-08-15 15:22:47 +0800

    地址更改

--- YunkuJavaSDK/src/com/yunkuent/sdk/HostConifg.java
+++ YunkuJavaSDK/src/com/yunkuent/sdk/HostConifg.java
@@ -4,8 +4,8 @@ package com.yunkuent.sdk;
  * Created by Brandon on 2014/8/14.
  */
 interface HostConifg {
-     static  String OAUTH_HOST = "http://zka.goukuai.cn";
-    //    private static final String OAUTH_HOST = "http://a.goukuai.cn";
-     static  String LIB_HOST = "http://zka-lib.goukuai.cn";
-//    private static final String LIB_HOST = "http://a-lib.goukuai.cn";
+    //     static  String OAUTH_HOST = "http://zka.goukuai.cn";
+    static final String OAUTH_HOST = "http://a.goukuai.cn";
+    //     static  String LIB_HOST = "http://zka-lib.goukuai.cn";
+    static final String LIB_HOST = "http://a-lib.goukuai.cn";
 }
