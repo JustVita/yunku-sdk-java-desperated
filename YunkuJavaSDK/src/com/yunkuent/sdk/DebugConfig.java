0c8280b45972732577823fc966958c513aeeb3af
BrandonGk <zb@gokuai.com>
2015-05-11 14:17:06 +0800

    调整LogPrint日志输出的方式

--- YunkuJavaSDK/src/com/yunkuent/sdk/DebugConfig.java
+++ YunkuJavaSDK/src/com/yunkuent/sdk/DebugConfig.java
@@ -6,5 +6,17 @@ package com.yunkuent.sdk;
 public class DebugConfig {
 
     public static boolean PRINT_LOG = false;
+    public static int PRINT_LOG_TYPE = 0;
+    public static final int LOG_TYPE_MEMORY_HANDLER = 0;//在控制台里写日志，并写入文件
+    public static final int LOG_TYPE_DETECTOR = 1;//返回监听到的日志内容，自己做打日志处理
     public static String LOG_PATH = "D://";
+
+    public interface LogDetector {
+        void getLog(String message);
+    }
+
+    public static void setListener(LogDetector listener) {
+        LogPrint.setLogDector(listener);
+    }
+
 }
--- YunkuJavaSDK/src/com/yunkuent/sdk/LogPrint.java
+++ YunkuJavaSDK/src/com/yunkuent/sdk/LogPrint.java
@@ -9,24 +9,48 @@ class LogPrint {
 
     private static YunkuMemoryHandler mHander;
 
-    private synchronized static YunkuMemoryHandler getInstance() {
+    private synchronized static YunkuMemoryHandler getHandlerInstance() {
         if (mHander == null) {
             mHander = new YunkuMemoryHandler();
         }
         return mHander;
     }
 
+    private static DebugConfig.LogDetector mDectector;
+
     public static void print(String log) {
-        print(Level.INFO, log);
+        if (DebugConfig.PRINT_LOG) {
+            switch (DebugConfig.PRINT_LOG_TYPE) {
+                case DebugConfig.LOG_TYPE_MEMORY_HANDLER:
+                    print(Level.INFO, log);
+                    break;
+                case DebugConfig.LOG_TYPE_DETECTOR:
+                    if (mDectector != null) {
+                        mDectector.getLog(log);
+                    }
+                    break;
+            }
+        }
     }
 
 
     public static void print(Level level, String log) {
-        if (DebugConfig.PRINT_LOG) {
-            YunkuMemoryHandler mt = getInstance();
-            // 在MemoryHandler中缓存日志记录
-            mt.getLogger().log(level, log);
-            mt.getMhandler().push();
+        switch (DebugConfig.PRINT_LOG_TYPE) {
+            case DebugConfig.LOG_TYPE_DETECTOR:
+                break;
+            case DebugConfig.LOG_TYPE_MEMORY_HANDLER:
+                YunkuMemoryHandler mt = getHandlerInstance();
+                // 在MemoryHandler中缓存日志记录
+                mt.getLogger().log(level, log);
+                mt.getMhandler().push();
+                break;
         }
+
     }
+
+    public static void setLogDector(DebugConfig.LogDetector detector) {
+        mDectector = detector;
+    }
+
+
 }
