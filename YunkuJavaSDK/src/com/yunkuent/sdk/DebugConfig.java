57dd7c2517031af3c716f8c166e99085d8c5444b
wink <winkrie@163.com>
2016-11-09 15:19:25 +0800

    fix log file bug

--- YunkuJavaSDK/src/com/yunkuent/sdk/DebugConfig.java
+++ YunkuJavaSDK/src/com/yunkuent/sdk/DebugConfig.java
@@ -1,5 +1,7 @@
 package com.yunkuent.sdk;
 
+import java.util.logging.Level;
+
 /**
  * 调试配置
  */
@@ -9,10 +11,10 @@ public class DebugConfig {
     public static int PRINT_LOG_TYPE = 0;
     public static final int LOG_TYPE_MEMORY_HANDLER = 0;//在控制台里写日志，并写入文件
     public static final int LOG_TYPE_DETECTOR = 1;//返回监听到的日志内容，自己做打日志处理
-    public static String LOG_PATH = "D://";
+    public static String LOG_PATH = "";
 
     public interface LogDetector {
-        void getLog(String message);
+        void getLog(Level level, String message);
     }
 
     public static void setListener(LogDetector listener) {
